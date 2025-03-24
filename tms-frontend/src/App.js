631108ebec36566c5a3cc501b232ce7d07e90e47
Usman <usmanmohsin242@gmail.com>
2025-03-24 14:43:15 +0500

    Initial React frontend setup

---
+++ tms-frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello, TMS!</h1>
+      <p>Welcome to your Transportation Management System.</p>
+    </div>
+  );
+}
+
+export default App;
